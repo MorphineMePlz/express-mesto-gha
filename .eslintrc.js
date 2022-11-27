64c39c43c7602a217e65ca9faad8cc76dea73ddb
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-11-27 16:14:48 +0600

    fix: eslint

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    'no-console': 'off',
-    'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'linebreak-style': 0,
-    indent: [2, 2],
-  },
-};
